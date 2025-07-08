485e1303ab188003fb2155d7e4a60347cddd9ac6
kotodzukuri <kotodzukuri@outlook.jp>
2025-07-08 14:54:06 +0900

    Add BMI課題のファイル

---
+++ kadai_005/bmi.js
@@ -0,0 +1,9 @@
+let weight = 68;     // 体重（kg）
+let height = 1.7;    // 身長（m）
+
+let bmi = weight / (height * height);
+
+console.log("体重：" + weight + "kg");
+console.log("身長：" + height + "m");
+console.log("BMIは " + bmi.toFixed(2) + " です。");
+
